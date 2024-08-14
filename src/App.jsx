3c0a93d3d5e43de9bccb33b78ac2c8e06b3fad12
tofayeLL <ahmedtofayel132@gmail.com>
2024-08-14 22:21:50 +0600

    INstall initial packages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-5xl font-bold bg-red-400'>StyleSavvy</h1>
+      <button className='btn btn-secondary'>Daisy</button>
+    
+    </>
+  )
+}
+
+export default App
