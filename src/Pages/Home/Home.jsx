44c4b461bc2f34bfa5cb8c6ad4dec798f7e9040d
tofayeLL <ahmedtofayel132@gmail.com>
2024-08-16 01:46:16 +0600

    finished design card and display all the products card

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
+import { BsCurrencyDollar } from "react-icons/bs";
 
 
 const Home = () => {
@@ -49,7 +53,7 @@ const Home = () => {
 
 
 
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-4">
                     {
                         allProducts.map((item, index) => <div key={item._id} className="flex flex-col bg-gray-100 pb-8 b border-[1px] border-slate-100 hover:shadow-xl">
 
@@ -58,17 +62,40 @@ const Home = () => {
                             </h4>
 
                             <div className="flex flex-col flex-1 p-4">
-                                <p>{item.productName}</p>
+                                <p className="text-lg">{item.productName}</p>
 
-                                <p className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">{item.brandName}</p>
+                                <p className="text-xs tracking-wider uppercase hover:underline text-blue-500">{item.brandName}</p>
 
                                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">{item.description}</h3>
 
                                 <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-600">
-                                    <span>{item.ratings}</span>
-                                    <span>{item.createdDate}</span>
+                                    <span> <Rating
+                                        style={{ maxWidth: 100 }}
+                                        value={item.ratings}
+                                        readOnly
+                                    /></span>
+                                    <span className="text-base">{item.createdDate}</span>
                                 </div>
+
+                                <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-600">
+                                    <span className="flex justify-center items-center text-2xl text-black font-semibold"><BsCurrencyDollar></BsCurrencyDollar> {item.price}</span>
+                                    <span className="text-base text-blue-500">{item.category}</span>
+                                </div>
+
+
+
+
+
+
                             </div>
+
+
+
+
+
+
+
+
                         </div>)
                     }
 
