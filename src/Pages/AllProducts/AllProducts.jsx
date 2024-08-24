2e0afc4c0e44a08f73e028fe4da1726fe7fc0cb2
tofayeLL <ahmedtofayel132@gmail.com>
2024-08-24 23:13:14 +0600

    added show all button in all products page

--- src/Pages/AllProducts/AllProducts.jsx
+++ src/Pages/AllProducts/AllProducts.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { Rating } from "@smastrom/react-rating";
 import { BsCurrencyDollar } from "react-icons/bs";
+import { FaArrowRight } from "react-icons/fa";
 
 
 const AllProducts = () => {
@@ -14,11 +15,11 @@ const AllProducts = () => {
         queryFn: async () => {
             const data = await axiosPublic.get('/totalProducts');
             // console.log(data.data);
-            return data.data;
+            return data.data.slice(0, 12);
         }, initialData: []
     })
 
-   
+
 
 
 
@@ -27,12 +28,12 @@ const AllProducts = () => {
     return (
         <section className="pt-24">
             <p>{products.length}</p>
-            <div >
+            <div  className="px-4">
 
 
                 <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-4">
-                    { 
-                        products.map((item, index) =>  <div key={item._id} className="flex flex-col bg-gray-100 pb-8 border-[1px] border-slate-100 hover:shadow-xl">
+                    {
+                        products.map((item, index) => <div key={item._id} className="flex flex-col bg-gray-100 pb-8 border-[1px] border-slate-100 hover:shadow-xl">
 
                             <h4 >
                                 <img alt="" className="object-center object-cover w-full lg:h-96 h-96 dark:bg-gray-500" src={item.productImage} />
@@ -80,6 +81,13 @@ const AllProducts = () => {
 
                 </div>
 
+            </div>
+
+            <div className="py-8 px-4">
+                <button className="relative  items-center justify-start inline-block px-4 py-2 overflow-hidden font-medium transition-all hover:bg-gray-200 rounded-full bg-blue-500 group">
+                    <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all  border-gray-200 rounded-full"></span>
+                    <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-blue-600 flex justify-center items-center gap-2">Show All<span><FaArrowRight></FaArrowRight></span></span>
+                </button>
             </div>
 
         </section >
